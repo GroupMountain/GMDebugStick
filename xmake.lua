8b13033bea3889d7ca78752f2afadb5faa8ff54f
n15421 <119112174+n15421@users.noreply.github.com>
2025-06-02 23:32:44 +0800

    chore: adapt to gmlib 1.2.0-rc.1

--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,9 @@ add_rules("mode.debug", "mode.release")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("groupmountain-repo https://github.com/GroupMountain/xmake-repo.git")
 
-add_requires("levilamina 1.1.1", {configs = {target_type = "server"}})
-add_requires("levibuildscript 0.3.0")
-add_requires("gmlib 1.0.1")
+add_requires("levilamina 1.2.0", {configs = {target_type = "server"}})
+add_requires("levibuildscript 0.4.0")
+add_requires("gmlib 1.2.0-rc.1")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
