4abd1d28ab0d4931c3c172762dde1a67c22498ac
zimuya4153 <limulu10@163.com>
2025-10-23 20:27:22 +0800

    feat: adapt levilamina 1.6.x

--- xmake.lua
+++ xmake.lua
@@ -3,10 +3,10 @@ add_rules("mode.debug", "mode.release")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("groupmountain-repo https://github.com/GroupMountain/xmake-repo.git")
 
-add_requires("levilamina 1.5.2", {configs = {target_type = "server"}})
+add_requires("levilamina 1.6.1", {configs = {target_type = "server"}})
 add_requires("levibuildscript 0.5.2")
-add_requires("gmlib 1.5.2")
-add_requires("modapi 0.1.1")
+add_requires("gmlib 1.6.0")
+add_requires("modapi 0.2.1")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
